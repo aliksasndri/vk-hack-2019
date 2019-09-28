584c1b2b7452f81f73288de48c26face3e27b221
Ivan Smetanin <smetanin23@yandex.ru>
2019-09-28 15:47:45 +0300

    Add app icon, add start screen

---
+++ SunCity/User Stories/Start/View/StartViewController.swift
@@ -0,0 +1,40 @@
+//
+//  StartViewController.swift
+//  SunCity
+//
+//  Created by Ivan Smetanin on 28/09/2019.
+//  Copyright © 2019 Ivan Smetanin. All rights reserved.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController, StartModuleOutput {
+
+    // MARK: - Subviews
+
+    @IBOutlet weak var fillFormButton: UIButton!
+    @IBOutlet weak var enterButton: UIButton!
+
+    // MARK: - Properties
+
+    // MARK: - UIViewController
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .darkContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Internal helpers
+
+    // MARK: - Actions
+
+    @IBAction private func fill(_ sender: Any) {
+    }
+
+    @IBAction private func login(_ sender: Any) {
+    }
+
+}
