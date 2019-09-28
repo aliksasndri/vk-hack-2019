ac8e5adeec766361bebb83958a73f74ff8b647f8
Ivan Smetanin <smetanin23@yandex.ru>
2019-09-28 15:48:20 +0300

    Add root view controller setting

--- SunCity/Application/AppDelegate.swift
+++ SunCity/Application/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     private func makeRootViewController() -> UIViewController {
-        let controller = UIViewController()
+        let (controller, _) = StartModuleConfigurator().configure()
         return controller
     }
 
