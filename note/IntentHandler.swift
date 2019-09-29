2b4d3e2a955cc9813ea2233ea5f40f1f23d3a815
Anton Dryakhlykh <dryakhlykh@surfstudio.co>
2019-09-29 11:45:05 +0300

    Add handling.

--- note/IntentHandler.swift
+++ note/IntentHandler.swift
@@ -10,6 +10,9 @@ import Intents
 
 class IntentHandler: INExtension {
     override func handler(for intent: INIntent) -> Any {
+        if intent is INCreateNoteIntent {
+            return NoteHandler()
+        }
         return self
     }
     
@@ -17,6 +20,31 @@ class IntentHandler: INExtension {
 
 class NoteHandler: NSObject, INCreateNoteIntentHandling {
     func handle(intent: INCreateNoteIntent, completion: @escaping (INCreateNoteIntentResponse) -> Void) {
-        completion(INCreateNoteIntentResponse(code: .inProgress, userActivity: .none))
+
+        completion(INCreateNoteIntentResponse(code: .success, userActivity: .none))
+    }
+
+    func confirm(intent: INCreateNoteIntent, completion: @escaping (INCreateNoteIntentResponse) -> Swift.Void) {
+        completion(INCreateNoteIntentResponse(code: INCreateNoteIntentResponseCode.ready, userActivity: nil))
+    }
+
+    func resolveTitle(forCreateNote intent: INCreateNoteIntent, with completion: @escaping (INStringResolutionResult) -> Swift.Void) {
+        let result: INStringResolutionResult
+        if let title = intent.title?.spokenPhrase, title.count > 0 {
+            result = INStringResolutionResult.success(with: title)
+        } else {
+            result = INStringResolutionResult.needsValue()
+        }
+        completion(result)
+    }
+
+    func resolveContent(for intent: INCreateNoteIntent, with completion: @escaping (INNoteContentResolutionResult) -> Swift.Void) {
+        let result: INNoteContentResolutionResult
+        if let content = intent.content {
+            result = INNoteContentResolutionResult.success(with: content)
+        } else {
+            result = INNoteContentResolutionResult.notRequired()
+        }
+        completion(result)
     }
 }
